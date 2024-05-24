75968aa6b1595e24d8c6794a12364366ab6bcb1d
xAlpharax <42233094+xAlpharax@users.noreply.github.com>
2024-05-24 15:37:51 +0300

    Discord.py up and working with slash commands and proper environment.
    Changes to be committed:
        modified:   main.py
        modified:   requirements.txt

--- main.py
+++ main.py
@@ -1,3 +1,48 @@
+#!/usr/bin/env python3
+
+##########################################################################################
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DISCORD_TOKEN = str(os.getenv("DISCORD_TOKEN"))
+
+##########################################################################################
+
 import discord
 
-print("Hello World!")
+# from responses import get_response
+
+class CustomClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.all())
+        self.synced = False
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print(f"{self.user} has connected to Discord!")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        # if get_response(message.content):
+            # await message.channel.send(get_response(message.content))
+
+client = CustomClient()
+tree = discord.app_commands.CommandTree(client)
+
+##########################################################################################
+
+@tree.command(name="test", description="Command used for testing")
+async def test(interaction: discord.Interaction, name: str):
+    await interaction.response.send_message(f"Hello {name}. I was made with Discord.py!", ephemeral = True)
+
+##########################################################################################
+
+client.run(DISCORD_TOKEN)
+
+##########################################################################################
